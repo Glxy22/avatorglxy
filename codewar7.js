8a78335a5f0ecdb861373be0f422075cb69d4812
Nadia Shahid <113157154+Glxy22@users.noreply.github.com>
2022-12-26 08:00:21 -0600

    number from string and convert to number

---
+++ codewar7.js
@@ -0,0 +1,7 @@
+unction getAge(inputString){
+// return the girl's correct age as an integer. 
+  const str = inputString.split('');
+   return parseInt(str[0]);
+  // retrun age;
+}
+getAge("4 years old");
