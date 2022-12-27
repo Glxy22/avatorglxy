33585c00aa84d1b29df2c15273ca901cd044a493
Nadia Shahid <113157154+Glxy22@users.noreply.github.com>
2022-12-26 22:27:46 -0600

    lower and uper case letter conversion

---
+++ codewar10.js
@@ -0,0 +1,14 @@
+
+//  each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. 
+
+String.prototype.toAlternatingCase = function () {
+let str = '';
+  for(let i = 0; i< this.length; i++){
+    if (this[i] == this[i].toLowerCase()){ 
+      str += this[i].toUpperCase();
+    } else {
+      str += this[i].toLowerCase();
+    }
+  }
+  return str;
+}
