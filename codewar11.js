92fbbc1b4a2c648728744c0002ce42d2c72180df
Nadia Shahid <113157154+Glxy22@users.noreply.github.com>
2022-12-29 11:35:25 -0600

    return number only from array

---
+++ codewar11.js
@@ -0,0 +1,11 @@
+//return number only from array
+const l=[1, 5 ,"hee" , "go", 5];
+let arr= [];
+  let i=0;
+  for( let x of l){
+    if((typeof(x) !=='string'))
+      //if(typeof(l[x]) !=="undefined")
+      arr.push(x);
+  }
+  return arr;
+    
